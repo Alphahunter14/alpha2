17cd4e1172d099974c6e8b07f045300f41590660
Alphahunter14 <catharva14@gmail.com>
2024-05-04 17:04:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import io
 import os
 
 app = Flask(__name__)
+app = Flask(__name__, static_url_path='../Templates/index.html')
+
 
 temp_dir = os.path.join(app.root_path, 'temp')
 os.makedirs(temp_dir, exist_ok=True)
